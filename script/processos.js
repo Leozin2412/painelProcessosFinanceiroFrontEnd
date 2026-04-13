9d0b32ab09cb0180893752b1dc959741caeb692c
Leonardo Monteiro <leonardommonteiro2004@gmail.com]>
2026-04-13 14:12:12 -0300

    agora vai22222222222

--- script/processos.js
+++ script/processos.js
@@ -953,7 +953,7 @@ async function handleSubmitBH(e) {
   btn.innerHTML = `<svg class="w-5 h-5 animate-spin mr-2 inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg> Gerando...`;
 
   try {
-    const response = await fetch('/automacao/ts/export', {
+    const response = await fetch('https://painelprocessosfinanceirobackend.onrender.com/automacao/ts/export', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
